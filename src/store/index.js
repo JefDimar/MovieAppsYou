60e85ec77b9518cd595a6850b891cdcbedf9100d
JefDimar <jefdimar@gmail.com>
2021-03-04 12:29:06 +0700

    init redux-thunk and applying in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import movieReducer from '../reducers/moviesReducer'
 import favoriteReducer from '../reducers/favoritesReducer'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
   movie: movieReducer,
   favorite: favoriteReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
