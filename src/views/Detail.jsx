a27442cba6be185affd32245930c31974fdb1dfb
JefDimar <jefdimar@gmail.com>
2021-03-04 15:47:22 +0700

    fixing layout in detailPage

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -20,38 +20,41 @@ export default function Detail() {
       `/videos?api_key=${process.env.REACT_APP_KEY}&language=en-US`
   );
   useEffect(() => {
-    setLoading(Array.isArray(data) && Array.isArray(trailers))
+    setLoading(Array.isArray(data) && Array.isArray(trailers));
     setTrailer(trailers);
     setMovie(data);
   }, [data, trailers]);
   return (
-    <div className="mt-3 offset-3 d-flex flex-row">
+    <div className="mt-3 container row">
       {loading ? (
         <h1>Please wait a sec...</h1>
       ) : (
         <>
-        <div className="float-left">
-          <h1>{movie.title}</h1>
-          <Card className="w-50 h-50">
-            <Card.Img
-              variant="top"
-              src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+          <div className="text-center offset-1 mb-3">
+            <h1>{movie.title}</h1>
+          </div>
+          <div className="float-left offset-2 col">
+            <Card className="w-100 h-50">
+              <Card.Img
+                variant="top"
+                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+              />
+              <Card.Body className="text-reset">
+                <Card.Title>Synopsis</Card.Title>
+                <Card.Text>{movie.overview}</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="float-right col">
+            <h2>Trailer: </h2>
+            <iframe
+              src={`https://www.youtube.com/embed/${trailer?.results?.[0]?.key}?autoplay=1`}
+              title={trailer?.results?.[0]?.name}
+              width="500"
+              height="350"
+              className="m-3"
             />
-            <Card.Header className="text-center">{movie.title}</Card.Header>
-            <Card.Body className="text-reset">
-              <Card.Title>Synopsis</Card.Title>
-              <Card.Text>{movie.overview}</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="float-right">
-          <iframe
-            src={`https://www.youtube.com/embed/${trailer?.results?.[0]?.key}?autoplay=1`}
-            title={trailer?.results?.[0]?.name}
-            width="420" height="315"
-            className="m-3"
-          />
-        </div>
+          </div>
         </>
       )}
     </div>
