3f37d01ccdd6ad625d49919e378ee51bcef4e80a
JefDimar <jefdimar@gmail.com>
2021-03-04 16:24:16 +0700

    adding feature search w/ debounce implemented in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import logo from "../logo.svg";
 import { NavLink } from "react-router-dom";
+import useDebounce from "../hooks/useDebounce";
+import { searchMovies, fetchingMovies } from "../store/actions";
+import { useDispatch } from "react-redux";
 
 export default function Navigation() {
+  const dispatch = useDispatch();
+  const [word, setWord] = useState("");
+  const data = useDebounce(word, 500);
+  // console.log(data)
+  const search = (e) => {
+    // console.log(e.target.value)
+    setWord(e.target.value);
+  };
+  const findMovie = (e) => {
+    e.preventDefault();
+    // https://api.themoviedb.org/3/search/movie?api_key=c2dcee8f08e877d5fb3559af163b7e36&query=ko
+    const query = data.split(" ").join("%20");
+    console.log(query);
+    if (!query) {
+      dispatch(fetchingMovies());
+    } else {
+      dispatch(searchMovies(query));
+    }
+  };
   return (
     <div>
       <Navbar bg="dark" fixed="top">
@@ -28,11 +50,12 @@ export default function Navigation() {
               Favorite
             </NavLink>
           </Nav>
-          <form className="d-flex flex-row">
+          <form onSubmit={findMovie} className="d-flex flex-row">
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
+              onChange={search}
             />
             <button className="btn btn-success" type="submit">
               Search
--- src/store/actions.js
+++ src/store/actions.js
@@ -16,3 +16,11 @@ export function fetchingMovies() {
       .then(data => dispatch(fetchMovies(data.results)))
   }
 }
+
+export function searchMovies(query) {
+  return (dispatch) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_KEY}&query=${query}`)
+      .then(response => response.json())
+      .then(data => dispatch(fetchMovies(data.results)))
+  }
+}
